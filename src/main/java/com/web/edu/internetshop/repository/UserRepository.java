8661c67fe57ef9cbf93cfd81f4cfc95844890ab5
Пользователь <Nazar.sekh@gmail.com>
2018-11-17 01:22:57 +0300

    fix

--- src/main/java/com/web/edu/internetshop/repository/UserRepository.java
+++ src/main/java/com/web/edu/internetshop/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.web.edu.internetshop.repository;
 
 import com.web.edu.internetshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
